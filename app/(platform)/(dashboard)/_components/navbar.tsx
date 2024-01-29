e341bba906a1485c5a1ca6c2a611eef68dfced54
Rafal Szostak <rafal.szostak2@gmail.com>
2024-01-29 21:35:38 +0100

    feat: Add board view, add board navbar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -4,6 +4,7 @@ import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 import { Plus } from "lucide-react"
 import React from "react"
 import MobileSidebar from "./mobile-sidebar"
+import { FormPopover } from "@/components/form/form-popover"
 
 const Navbar = () => {
   return (
@@ -13,20 +14,24 @@ const Navbar = () => {
         <div className="hidden md:flex">
           <Logo />
         </div>
-        <Button
-          size="sm"
-          className="rounded-sm hidden md:block h-auto py-1.5 px-2"
-          variant="primary"
-        >
-          Create
-        </Button>
-        <Button
-          size="sm"
-          className="rounded-sm block md:hidden"
-          variant="primary"
-        >
-          <Plus className="h-4 w-4" />
-        </Button>
+        <FormPopover align="start" side="bottom" sideOffset={18}>
+          <Button
+            size="sm"
+            className="rounded-sm hidden md:block h-auto py-1.5 px-2"
+            variant="primary"
+          >
+            Create
+          </Button>
+        </FormPopover>
+        <FormPopover>
+          <Button
+            size="sm"
+            className="rounded-sm block md:hidden"
+            variant="primary"
+          >
+            <Plus className="h-4 w-4" />
+          </Button>
+        </FormPopover>
       </div>
       <div className="ml-auto flex items-center gap-x-2">
         <OrganizationSwitcher
---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,40 @@
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs"
+import { notFound, redirect } from "next/navigation"
+import { BoardNavbar } from "./_components/board-navbar"
+
+const BoardIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: { boardId: string }
+}) => {
+  const { orgId } = auth()
+  if (!orgId) {
+    redirect("/select-org")
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  })
+
+  if (!board) {
+    notFound()
+  }
+
+  return (
+    <div
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+      className="relative h-full bg-no-repeat bg-cover bg-center"
+    >
+      <BoardNavbar data={board} />
+      <div className="absolute inset-0 bg-black/10" />
+      <main className="relative pt-28 h-full">{children}</main>
+    </div>
+  )
+}
+export default BoardIdLayout
