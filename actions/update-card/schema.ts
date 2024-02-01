2e98558e9e782d0b28df1d9086574d9c063714f6
Rafal Szostak <rafal.szostak2@gmail.com>
2024-02-01 17:09:37 +0100

    feat: Add card item modal edit

---
+++ actions/update-card/schema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod"
+
+export const UpdateCard = z.object({
+  title: z.optional(
+    z
+      .string({
+        required_error: "Title is rquired",
+        invalid_type_error: "Title is required",
+      })
+      .min(3, {
+        message: "Title is too short",
+      })
+  ),
+  id: z.string(),
+  boardId: z.string(),
+  description: z.optional(
+    z
+      .string({
+        required_error: "Description is rquired",
+        invalid_type_error: "Description is required",
+      })
+      .min(3, {
+        message: "Description is too short",
+      })
+  ),
+})
